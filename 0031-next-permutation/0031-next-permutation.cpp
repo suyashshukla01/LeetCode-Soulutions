9d08e6e58a718879e3052b5cd463af54e66fd58d
suyashshukla01 <suyashshukla9245@gmail.com>
2026-06-28 01:11:11 +0530

    STL use
    Implement next permutation algorithm without STL.

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin() , nums.end());
+
+        // next_permutation(nums.begin() , nums.end());
+        // we can solve this problem using this C++ STL too.
+        
+        int pivot = -1 , n = nums.size();
+
+        for (int i = n-2 ; i >= 0 ; i--) {
+            if (nums[i] < nums[i+1]) {
+                pivot = i;
+                break;
+            }
+        } 
+
+        if (pivot == -1) {
+            reverse(nums.begin() , nums.end());
+            return;
+        }
+
+        for (int i = n-1 ; i > pivot ; i--) {
+            if (nums[i] > nums[pivot]) {
+                swap (nums[i] , nums[pivot]);
+                break;
+            }
+        }
+        int i = pivot+1 , j = n-1;
+        while (i <= j){
+            swap(nums[i++] , nums[j--]);
+        }
     }
 };
