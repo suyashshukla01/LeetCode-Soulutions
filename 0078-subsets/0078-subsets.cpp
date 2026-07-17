6f468e46e44d46b33048a30f6b91f21a134579f7
suyashshukla01 <suyashshukla9245@gmail.com>
2026-07-17 22:45:51 +0530

    Time: 0 ms (100%), Space: 9.9 MB (82.4%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -3,7 +3,7 @@ public:
 
     void getAllSubsets(vector<int>& nums , vector<int>& ans , int i , vector<vector<int>>& allSubsets){
         if(i == nums.size()){
-            allSubsets.push_back({ans});
+            allSubsets.push_back(ans);
             return;
         }
 
